ffc07e23f196e2c94ddc205169979356fd065ea3
AbhiramGudapati <gudapatileelaabhiram@gmail.com>
2025-04-19 20:59:14 -0500

    Add files via upload

---
+++ Code Files/active_buzzer.ino
@@ -0,0 +1,17 @@
+int buzzerPin = 11;
+void setup() {
+  pinMode(buzzerPin, OUTPUT);
+  beep(50);
+  beep(50);
+  delay(1000);
+}
+void loop() {
+  beep(50);
+  delay(1000);
+}
+void beep(unsigned char delayms) {
+  analogWrite(buzzerPin, 20);
+  delay(delayms);
+  analogWrite(buzzerPin, 0);
+  delay(delayms);
+}
